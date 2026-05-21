4bd01dec9239f3039b4e3af8c355c10109d8625f
maxistabile <maxistabile@hotmail.com>
2026-05-21 05:31:58 -0300

    Estructura inicial de modelos y repositorio mock

---
+++ TUP_Materias/Data/MockRepository.cs
@@ -0,0 +1,41 @@
+﻿using TUP_Materias.Models;
+
+namespace TUP_Materias.Data
+{
+    public static class MockRepository
+    {
+        // La lista global que va a guardar las materias en la memoria RAM
+        public static List<Materia> Materias { get; set; } = new List<Materia>();
+
+        // El constructor estático que se ejecuta una sola vez al arrancar la app
+        static MockRepository()
+        {
+            // 1. Inventamos unos alumnos
+            var alumno1 = new Alumno { Id = 1, Nombre = "Maximiliano", Apellido = "Stabile" };
+            var alumno2 = new Alumno { Id = 2, Nombre = "Anahí", Apellido = "López" };
+            var alumno3 = new Alumno { Id = 3, Nombre = "Lucas", Apellido = "Fernández" };
+            var alumno4 = new Alumno { Id = 4, Nombre = "Bruno", Apellido = "Díaz" };
+
+            // 2. Inventamos los profesores
+            var profe1 = new Profesor { Id = 1, Nombre = "Carlos", Apellido = "Gómez" };
+            var profe2 = new Profesor { Id = 2, Nombre = "Patricia", Apellido = "Rodríguez" };
+
+            // 3. Armamos las materias y las metemos en la lista global
+            Materias.Add(new Materia
+            {
+                Id = 1,
+                Nombre = "Programación III",
+                ProfesorAsignado = profe1,
+                AlumnosInscriptos = new List<Alumno> { alumno1, alumno2, alumno3 }
+            });
+
+            Materias.Add(new Materia
+            {
+                Id = 2,
+                Nombre = "Base de Datos II",
+                ProfesorAsignado = profe2,
+                AlumnosInscriptos = new List<Alumno> { alumno1, alumno4 }
+            });
+        }
+    }
+}
---
+++ TUP_Materias/Models/Materia.cs
@@ -0,0 +1,10 @@
+﻿namespace TUP_Materias.Models
+{
+    public class Materia
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public Profesor ProfesorAsignado { get; set; }
+        public List<Alumno> AlumnosInscriptos { get; set; }
+    }
+}
